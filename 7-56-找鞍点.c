d4b10ba99862b5b158bb9dcf85ecbde278dbd649
许叶杨 <xuyeyang@outlook.com>
2019-09-22 22:35:54 +0800

    Done

--- 7-56-找鞍点.c
+++ 7-56-找鞍点.c
@@ -1,91 +1,36 @@
 /*================================================================
 *   Copyright (C) 2018 Navin Xu. All rights reserved.
 *   
-*   Filename    ：Week07-02-saddle-matrix.c
+*   Filename    ：7-56-找鞍点.c
 *   Author      ：Navin Xu
 *   E-Mail      ：admin@navinxu.com
-*   Create Date ：2018年12月30日
+*   Create Date ：2019年09月22日
 *   Description ：
-        鞍点（5分）
-        题目内容：
-        
-        给定一个n*n矩阵A。矩阵A的鞍点是一个位置（i，j），在该位置上的元素是第i行上的最大数，第j列上的最小数。一个矩阵A也可能没有鞍点。
-        
-        你的任务是找出A的鞍点。
-        
-        输入格式:
-        
-        输入的第1行是一个正整数n, （1<=n<=6），然后有n行，每一行有n个整数，同一行上两个整数之间有一个或多个空格。
-        
-        
-        
-        输出格式：
-        
-        对输入的矩阵，如果找到鞍点，就输出其下标。下标为两个数字，第一个数字是行号，第二个数字是列号，均从0开始计数。
-        
-        如果找不到，就输出
-        
-        NONE
-        
-        题目所给的数据保证了不会出现多个鞍点。
-        
-        
-        
-        输入样例：
-        
-        4 
-        
-        1 7 4 1 
-        
-        4 8 3 6 
-        
-        1 6 1 2 
-        
-        0 7 8 9
-        
-        
-        
-        输出样例：
-        
-        2 1
-        
-        
-        我的样例：
-        1 7 4 11
-        4 8 3 60
-        1 9 1 8
-        0 7 8 29
-        输出：
-        NONE
-        
-        1 7 4 11
-        4 8 3 60
-        1 9 1 10
-        0 7 8 29
-        输出：
-        2 3
-        
-        1 8 3
-        4 7 6
-        9 9 0
-        输出：
-        1 1
-        
-        0 0 0 0
-        0 0 0 0
-        0 0 0 0
-        0 0 0 0
-        
-        10 10 10 10
-        10 10 10 10
-        10 10 10 10
-        10 10 10 10
-        
-        以上两个测试点结果都为：
-        0 0
-        
-        
-        时间限制：500ms内存限制：32000kb
+    7-56 找鞍点 (20 point(s))
+    一个矩阵元素的“鞍点”是指该位置上的元素值在该行上最大、在该列上最小。
+    
+    本题要求编写程序，求一个给定的n阶方阵的鞍点。
+    
+    输入格式：
+    输入第一行给出一个正整数n（1≤n≤6）。随后n行，每行给出n个整数，其间以空格分隔。
+    
+    输出格式：
+    输出在一行中按照“行下标 列下标”（下标从0开始）的格式输出鞍点的位置。如果鞍点不存在，则输出“NONE”。题目保证给出的矩阵至多存在一个鞍点。
+    
+    输入样例1：
+    4
+    1 7 4 1
+    4 8 3 6
+    1 6 1 2
+    0 7 8 9
+    输出样例1：
+    2 1
+    输入样例2：
+    2
+    1 7
+    4 1
+    输出样例2：
+    NONE
 ================================================================*/
 #include <stdio.h>
 
@@ -144,20 +89,29 @@ int main() {
             }
         }
 
-        printf("\n==============\n");
-        for (int i = 0;i < n; i ++)
-            for (int j = 0; j < n; j ++)
-                printf("%d%c", row_max_point[i][j], (j == n - 1) ? '\n' : ' ');
-        //return 0;
+        //printf("\n==============\n");
+        //for (int i = 0;i < n; i ++)
+        //    for (int j = 0; j < n; j ++)
+        //        printf("%d%c", row_max_point[i][j], (j == n - 1) ? '\n' : ' ');
+        ////return 0;
 
-        printf("\n==============\n");
+        //printf("\n==============\n");
 
         int min_col = 0, min_row = 0;
+        int matrix2[n][n];
         // 此 for 有大 Bug
         for (int i = 0;i < n; i ++) {
             for (int j = 0; j < n; j ++) {
-                printf("%d%c", matrix[j][i], (j == n -1 ) ? '\n' : ' ');
-                if (matrix[j][i] < matrix[min_col][min_row]) {
+                //printf("%d%c", matrix[j][i], (j == n -1 ) ? '\n' : ' ');
+                matrix2[i][j] = matrix[j][i];
+            }
+        }
+
+        for (int i = 0;i < n; i ++) {
+            min_col = 0;
+            min_row = i;
+            for (int j = 0; j < n; j ++) {
+                if (matrix2[i][j] < matrix2[min_row][min_col]) {
                     min_row = i;
                     min_col = j;
                 }
@@ -178,12 +132,12 @@ int main() {
             }
         }
 
-        printf("\n==============\n");
-        for (int i = 0;i < n; i ++)
-            for (int j = 0; j < n; j ++)
-                printf("%d%c", col_min_point[i][j], (j == n - 1) ? '\n' : ' ');
+        //printf("\n==============\n");
+        //for (int i = 0;i < n; i ++)
+        //    for (int j = 0; j < n; j ++)
+        //        printf("%d%c", col_min_point[i][j], (j == n - 1) ? '\n' : ' ');
 
-        printf("\n++++++++++\n");
+        //printf("\n++++++++++\n");
         unsigned char found = 0;
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j < n ; j ++) {
@@ -200,11 +154,6 @@ int main() {
 
         if (!found)
             printf("NONE");
-
-
-        return 0;
     }
-
-
     return 0;
 }
